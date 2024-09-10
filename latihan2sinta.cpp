ccd4f74bd69eb2a03a2ebe4a038f932c19ddf252
Sinta Suwanda <sintasuwanda21@gmail.com>
2024-09-10 00:04:15 -0700

    Add files via upload
    luas persegi panjang

---
+++ latihan2sinta.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+	
+	int luas,panjang,lebar;
+	panjang=4;
+	lebar=6;
+	luas=panjang*lebar;
+	cout<<"LUAS PERSEGIPANJANG = "<<luas<<endl;
+	return 0;
+}
