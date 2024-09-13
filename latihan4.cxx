ad999c8f6036367174ca43be23bdc4308ce7b4de
Sinta Suwanda <sintasuwanda21@gmail.com>
2024-09-13 15:25:18 +0700

    Add files via upload
    Luas segitiga siku-siku

---
+++ latihan4.cxx
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{  
+    float luas,tinggi,alas;
+    cout<<"masukan nilai tinggi = ";
+    cin>>tinggi;
+    cout<<"masukan nilai alas = ";
+    cin>>alas;
+    luas=0.5*tinggi*alas;
+    cout<<"luas segitiga siku-siku = "<<luas<<endl;
+    return 0;
+}
