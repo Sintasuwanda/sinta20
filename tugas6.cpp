a858fa54d823da42a89dae5e6afd6689031d316e
Sinta Suwanda <sintasuwanda21@gmail.com>
2024-09-14 14:46:37 +0700

    Add files via upload
    Tugas

---
+++ tugas6.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+
+{
+	 cout <<  "PENJUALAN BARANG ELEKTRONIK" << endl; 
+	 char nabar[30];
+	 float harga,jumlahjual,totalharga,diskon,totalbayar=0;
+	 cout<<"NamaBarang = ";
+	 cin>>nabar;
+	 cout<<"Harga = ";
+	 cin>>harga;
+	 cout<<"Jumlah Jual = ";
+	 cin>>jumlahjual;
+	 cout<<setprecision(13);
+	 totalharga=harga*jumlahjual;
+	 cout<<"totalharga = "<<totalharga<<endl;
+	 diskon=0.1*totalharga;
+	 cout<<"Diskon = "<<diskon<<endl;
+	 totalbayar=totalharga-diskon;
+	 cout<<"Total Bayar = "<<totalbayar<<endl;
+	 return 0;
+}
