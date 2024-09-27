e5f61a778185f986d7bcb48adfd6d8bc4543d91e
Sinta Suwanda <sintasuwanda21@gmail.com>
2024-09-27 13:28:11 +0700

    Add files via upload
    3 dimensi for

---
+++ latihan11.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+	for (int i= 1; i<5; i++)
+	for (int j= 1; j<5; j++)
+	for (int k= 1; k<5; k++){
+		
+		cout<<"perulangan ke-"<<i<<","<<j<<","<<k<<endl;}
+		
+		return 0;
+	}
+	
+
